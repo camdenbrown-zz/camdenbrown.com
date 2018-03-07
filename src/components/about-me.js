9d3045bf6adb4ce573fbc8c9ae57ad7e73d36da8
Camden Brown <camden.brown@metova.com>
2018-03-07 14:17:32 -0600

    add tooltip css

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -5,42 +5,43 @@ class AboutMe extends Component {
     return (
       <div className={'subject'}>
         <p className={'description'}>
-          A self taught jack-of-all-trades. I love to program and learn all types of things from JavaScript to how to implement DevOps tools. Check out some of the
+          A self taught jack-of-all-trades. I love to program and learn all types of things from JavaScript to how to
+          implement DevOps tools. Check out some of the
           things I dabble in below and what I aspire to learn.
         </p>
 
         <div className={'learning-container'}>
           <h4>Some of the things I have worked with:</h4>
           <div className={"icon-container"}>
-            <i className={"icon-ruby"}></i>
-            <i className={"icon-ruby-on-rails"}></i>
-            <i className={"icon-reactjs"}></i>
-            <i className={"icon-jquery"}></i>
-            <i className={"icon-angular"}></i>
-            <i className={"icon-mysql"}></i>
-            <i className={"icon-postgres"}></i>
-            <i className={"icon-redis"}></i>
-            <i className={"icon-aws"}></i>
-            <i className={"icon-grunt"}></i>
-            <i className={"icon-sass"}></i>
-            <i className={"icon-html5"}></i>
-            <i className={"icon-css3"}></i>
-            <i className={"icon-javascript"}></i>
-            <i className={"icon-perl"}></i>
+            <i data-tooltip="Ruby" className={"icon-ruby"}></i>
+            <i data-tooltip="Ruby on Rails" className={"icon-ruby-on-rails"}></i>
+            <i data-tooltip="ReactJS" className={"icon-reactjs"}></i>
+            <i data-tooltip="jQuery" className={"icon-jquery"}></i>
+            <i data-tooltip="AngularJs & Angular" className={"icon-angular"}></i>
+            <i data-tooltip="MySQL" className={"icon-mysql"}></i>
+            <i data-tooltip="PostGres" className={"icon-postgres"}></i>
+            <i data-tooltip="Redis" className={"icon-redis"}></i>
+            <i data-tooltip="AWS" className={"icon-aws"}></i>
+            <i data-tooltip="Grunt" className={"icon-grunt"}></i>
+            <i data-tooltip="SASS" className={"icon-sass"}></i>
+            <i data-tooltip="HTML5" className={"icon-html5"}></i>
+            <i data-tooltip="CSS3" className={"icon-css3"}></i>
+            <i data-tooltip="JavaScript" className={"icon-javascript"}></i>
+            <i data-tooltip="Perl" className={"icon-perl"}></i>
           </div>
         </div>
 
         <div className={'learning-container'}>
           <h4>Some of the things I want to learn:</h4>
           <div className={'icon-container'}>
-            <i className={"icon-docker"}></i>
-            <i className={"icon-elixir"}></i>
-            <i className={"icon-go"}></i>
-            <i className={"icon-python"}></i>
-            <i className={"icon-erlang"}></i>
-            <i className={"icon-mongodb"}></i>
-            <i className={"icon-cassandra"}></i>
-            <i className={"icon-nginx-alt"}></i>
+            <i data-tooltip="Docker" className={"icon-docker"}></i>
+            <i data-tooltip="Elixir" className={"icon-elixir"}></i>
+            <i data-tooltip="Go" className={"icon-go"}></i>
+            <i data-tooltip="Python" className={"icon-python"}></i>
+            <i data-tooltip="Erlang" className={"icon-erlang"}></i>
+            <i data-tooltip="MongoDB" className={"icon-mongodb"}></i>
+            <i data-tooltip="Cassandra" className={"icon-cassandra"}></i>
+            <i data-tooltip="Nginx" className={"icon-nginx-alt"}></i>
           </div>
         </div>
       </div>
